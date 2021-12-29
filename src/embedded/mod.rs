cb807d40b52b725611b2700f77cedad69f03a8aa
Lainera <fistlain@hotmail.com>
2021-12-29 14:47:01 -0700

    feat/generic
    Cleanup owned
    Rename bin serialization functions
    
    Signed-off-by: Lainera <fistlain@hotmail.com>

--- src/embedded/mod.rs
+++ src/embedded/mod.rs
@@ -1,11 +1,11 @@
-use crate::{Command, CommandError, read_as_u16, read_colour};
+use crate::{Command, CommandError, try_read_u16, try_read_colour};
 use core::convert::TryFrom;
 
 impl<'a> TryFrom<&'a [u8]> for Command<&'a [u8]> {
     type Error = CommandError;
 
     fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
-        if value.len() == 0 {
+        if value.is_empty() {
             return Err(CommandError::MalformedPayload);
         }
 
@@ -19,16 +19,16 @@ impl<'a> TryFrom<&'a [u8]> for Command<&'a [u8]> {
                 }
             }
             b'c' => {
-                let led_count = read_as_u16(&buffer[..2])?;
-                let colour = read_colour(&buffer[2..])?;
+                let led_count = try_read_u16(&buffer[..2])?;
+                let colour = try_read_colour(&buffer[2..])?;
                 Ok(Command::Constant { led_count, colour })
             }
             b'p' => {
-                let led_count = read_as_u16(&buffer[..2])?;
-                let start_led = read_colour(&buffer[2..5])?;
-                let end_led = read_colour(&buffer[5..8])?;
+                let led_count = try_read_u16(&buffer[..2])?;
+                let start_led = try_read_colour(&buffer[2..5])?;
+                let end_led = try_read_colour(&buffer[5..8])?;
                 let frames = buffer[8];
-                let period = read_as_u16(&buffer[9..11])?;
+                let period = try_read_u16(&buffer[9..11])?;
 
                 Ok(Command::Pulse {
                     start: start_led,
--- src/owned/mod.rs
+++ src/owned/mod.rs
@@ -1,49 +1,25 @@
 #![cfg(feature = "owned")]
 extern crate std;
 use std::vec::Vec;
-use crate::{Command, CommandError, read_as_u16, read_colour};
+use crate::{Command, CommandError};
 use core::convert::TryFrom;
+
+impl<'a> From<Command<&'a [u8]>> for Command<Vec<u8>> {
+   fn from(src: Command<&'a [u8]>) -> Self {
+       match src {
+        Command::Health => Command::Health,
+        Command::Constant { led_count, colour } => Command::Constant { led_count, colour },
+        Command::Stream(bytes) => Command::Stream(bytes.to_vec()),
+        Command::Pulse { led_count, start, end, frames, period } => Command::Pulse { led_count, start, end, frames, period },
+    }
+   } 
+}
 
 impl<'a> TryFrom<&'a [u8]> for Command<Vec<u8>> {
     type Error = CommandError;
-
     fn try_from(value: &'a [u8]) -> Result<Self, Self::Error> {
-        if value.is_empty() {
-            return Err(CommandError::MalformedPayload);
-        }
-
-        let (header, buffer) = value.split_at(1);
-        match header[0] {
-            b's' => {
-                if buffer.len() % 3 != 0 {
-                    Err(CommandError::MalformedPayload)
-                } else {
-                    Ok(Command::Stream(buffer.to_vec()))
-                }
-            }
-            b'c' => {
-                let led_count = read_as_u16(&buffer[..2])?;
-                let colour = read_colour(&buffer[2..])?;
-                Ok(Command::Constant { led_count, colour })
-            }
-            b'p' => {
-                let led_count = read_as_u16(&buffer[..2])?;
-                let start_led = read_colour(&buffer[2..5])?;
-                let end_led = read_colour(&buffer[5..8])?;
-                let frames = buffer[8];
-                let period = read_as_u16(&buffer[9..11])?;
-
-                Ok(Command::Pulse {
-                    start: start_led,
-                    end: end_led,
-                    frames,
-                    period,
-                    led_count,
-                })
-            }
-            b'h' => Ok(Command::Health),
-            _ => Err(CommandError::InvalidHeader),
-        }
+        Command::try_from(value)
+            .map(|cmd: Command<&[u8]>| cmd.into())
     }
 }
 
